884ffc3435edcce1b9b9269a324dc33ee796fa1e
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-08 21:53:21 -0500

    configure cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+  app.enableCors(CORS);
   app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
