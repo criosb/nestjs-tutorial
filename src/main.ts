cfcb27aeb357bbbac0a9cf2060e9f82f12e1d653
Carlos Rios <carios@belcorp.biz>
2023-10-08 21:34:08 -0500

    install and config logger (morgan)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
