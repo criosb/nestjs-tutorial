246be71c1a645692aff029bff267f2a19ff4c6a9
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-08 22:55:53 -0500

    configure typeorm module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { DataSourceConfig } from './config/data.source';
 
 @Module({
   imports: [
@@ -8,6 +10,7 @@ import { UsersModule } from './users/users.module';
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
     UsersModule,
   ],
 })
