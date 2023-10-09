f859a3318e1cce4fd2de4edd243b69970acf0ec3
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-09 00:18:37 -0500

    update references in entities files

--- src/projects/entities/projects.entities.ts
+++ src/projects/entities/projects.entities.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, OneToMany } from 'typeorm';
-import { BaseEntity } from 'src/config/base.entity';
-import { IProject } from 'src/interfaces/project.interface';
-import { UsersProjectsEntity } from 'src/users/entities/usersprojects.entity';
+import { BaseEntity } from '../../config/base.entity';
+import { IProject } from '../../interfaces/project.interface';
+import { UsersProjectsEntity } from '../../users/entities/usersprojects.entity';
 
 @Entity({ name: 'projects' })
 export class ProjectsEntity extends BaseEntity implements IProject {
--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, OneToMany } from 'typeorm';
-import { BaseEntity } from 'src/config/base.entity';
-import { ROLES } from 'src/constants/roles';
-import { IUser } from 'src/interfaces/users.interface';
+import { BaseEntity } from '../../config/base.entity';
+import { ROLES } from '../../constants/roles';
+import { IUser } from '../../interfaces/users.interface';
 import { UsersProjectsEntity } from './usersprojects.entity';
 
 @Entity({ name: 'users' })
