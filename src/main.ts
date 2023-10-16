1d9ad6ebdb9fe5b308af2a1ddfbe3953c9e79c12
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-16 14:05:02 -0500

    install and configura class validator

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,27 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as morgan from 'morgan';
 import { CORS } from './constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(morgan('dev'));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   app.enableCors(CORS);
   app.setGlobalPrefix('api');
+
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
+
   console.info(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
