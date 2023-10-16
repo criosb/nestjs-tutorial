c5958680c6328cad804bac8e9af915e205e07cf3
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-16 14:29:44 -0500

    Configure users service and module to use user repository

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersEntity } from '../entities/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly userRepository: Repository<UsersEntity>,
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
