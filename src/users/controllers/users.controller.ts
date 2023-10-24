b9fbc55a2930ac014fa44563353c82dbb53912e1
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-23 20:30:51 -0500

    implement all users controller operations

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from '../services/users.service';
+import { UpdateUserDTO, UserDTO } from '../dto/user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
-  @Get('hello')
-  getHello(): string {
-    return this.userService.getHello();
+  @Post('register')
+  public async registerUser(@Body() body: UserDTO) {
+    return await this.userService.createUser(body);
+  }
+
+  @Get('all')
+  public async findAllUsers() {
+    return await this.userService.findUsers();
+  }
+
+  @Get(':id')
+  public async findUserById(@Param('id') id: string) {
+    return await this.userService.findUserById(id);
+  }
+
+  @Patch(':id')
+  public async updateUser(
+    @Body() body: UpdateUserDTO,
+    @Param('id') id: string,
+  ) {
+    return await this.userService.updateUser(body, id);
+  }
+
+  @Delete(':id')
+  public async deleteUser(@Param('id') id: string) {
+    return await this.userService.deleteUser(id);
   }
 }
