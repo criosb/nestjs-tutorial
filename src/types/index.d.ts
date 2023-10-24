07696e0b7023c568393556c6d351beb1c8d71d20
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-24 13:42:38 -0500

    Config env variables in ProcessEnv

---
+++ src/types/index.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    PORT: number;
+    DB_HOST: string;
+    DB_PORT: number;
+    DB_NAME: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+  }
+}
