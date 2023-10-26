b453abdba1bd230bd0025e808160994c282a45a6
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-26 13:56:50 -0500

    add roles guard to users controller

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -10,23 +10,26 @@ import {
 } from '@nestjs/common';
 import { UsersService } from '../services';
 import { UpdateUserDTO, UserDTO, UserProjectAssignmentDTO } from '../dto';
-import { ACCESS_LEVEL } from '../../constants';
+import { ACCESS_LEVEL, ROLES } from '../../constants';
 import { UsersEntity } from '../entities';
 import { ProjectsEntity } from '../../projects/entities';
 import { AuthGuard } from '../../auth/guards/auth.guard';
-import { PublicAccess } from '../../auth/decorators/public.decorator';
+import { AdminAccess, Roles } from '../../auth/decorators';
+import { RolesGuard } from '../../auth/guards';
 
 @Controller('users')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Post('')
+  @AdminAccess()
   public async createUser(@Body() body: UserDTO) {
     return await this.userService.createUser(body);
   }
 
   @Post(':id/projects/:projectId')
+  @AdminAccess()
   public async assignUserToProject(
     @Body() { accessLevel }: { accessLevel: ACCESS_LEVEL },
     @Param('id') id: string,
@@ -45,17 +48,19 @@ export class UsersController {
   }
 
   @Get('all')
+  @Roles(ROLES.BASIC)
   public async getAllUsers() {
     return await this.userService.getAllUsers();
   }
 
   @Get(':id')
-  @PublicAccess()
+  @Roles(ROLES.BASIC)
   public async getUserById(@Param('id') id: string) {
     return await this.userService.getUserById(id);
   }
 
   @Patch(':id')
+  @AdminAccess()
   public async updateUser(
     @Body() body: UpdateUserDTO,
     @Param('id') id: string,
@@ -64,6 +69,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @AdminAccess()
   public async deleteUser(@Param('id') id: string) {
     return await this.userService.deleteUser(id);
   }
