bc3b73412b8bf02c36e345a2b5153e5980c5c52e
Carlos Rios Barinotto <crbarinotto@gmail.com>
2023-10-26 22:11:20 -0500

    make users module global

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,9 @@ import { Global, Module } from '@nestjs/common';
 import { AuthService } from './services';
 import { AuthController } from './controllers';
 import { UsersService } from '../users/services';
-import { UsersModule } from 'src/users';
 
 @Global()
 @Module({
-  imports: [UsersModule],
   providers: [AuthService, UsersService],
   controllers: [AuthController],
   exports: [AuthService],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { UsersService } from './services';
 import { UsersController } from './controllers';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity, UsersProjectsEntity } from './entities';
 
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([UsersEntity, UsersProjectsEntity])],
   providers: [UsersService],
